0181a7884cdfd4b5802beed7855e4a84e1342f00
Tristan Pourcelot <tristan.pourcelot@gmail.com>
2011-11-15 16:58:28 +0100

    First commit

---
+++ tri_fusion_squelette.c
@@ -0,0 +1,71 @@
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+typedef struct t_liste
+{
+  int valeur;
+  struct t_liste *suivant;
+}liste;
+
+
+liste *initialiser(double r, liste *l)
+{
+  //initialise la liste chaînée 
+
+}
+
+void afficher(liste *l)
+{
+  //affiche le contenu d'une liste
+
+}
+
+void supprimer_liste(liste *l)
+{
+  //libère la mémoire utilisée par la liste
+  //vous pouvez utiliser valgrind pour vérifier que vous avez bien tout libéré
+
+}
+
+liste* separation(liste *l)
+{
+  //sépare l en deux listes de même taille (+/- un élément)
+  //une de ces listes est retournée par la fonction, l'autre et la liste l modifiée
+
+}
+
+liste* fusion(liste *l1, liste *l2)
+{
+  //fusionne les listes triées l1 et l2 en une seule retournée par la fonction
+
+}
+
+liste *tri_fusion(liste *l)
+{
+  //algorithme de tri fusion qui sépare la liste en deux, tri récursivement chacune des listes et les fusionne
+
+}
+
+int main (int argc, char *argv[])
+{
+  //initialisation du générateur de nombres aléatoires
+  srand(time(NULL));
+  //variables pour mesurer le temps
+  long clk_tck = CLOCKS_PER_SEC;
+  clock_t t1, t2;
+  //heure avant le calcul
+  t1 = clock();
+  //le calcul dont on veut mesurer le temps
+
+  //heure après le calcul
+  t2 = clock();
+  (void)printf("Nb ticks/seconde = %ld,  Nb ticks depart : %ld, "
+	       "Nb ticks final : %ld\n",
+	       clk_tck, (long)t1, (long)t2);
+  (void)printf("Temps consomme (s) : %lf \n",
+	       (double)(t2-t1)/(double)clk_tck);
+
+}
